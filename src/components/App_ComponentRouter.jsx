b9741e359df26a8f81bf89b95d6dc5fb004897a9
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-09-21 12:03:45 +1000

    feat: nested route

--- src/components/App_ComponentRouter.jsx
+++ src/components/App_ComponentRouter.jsx
@@ -4,26 +4,25 @@ import HomePage from '../pages/HomePage';
 import AboutPage from '../pages/AboutPage';
 import ContactPage from '../pages/ContactPage';
 import Navbar from './Navbar';
-
+import ProjectLayout from '../pages/_templates/ProjectLayout';
+import PokemonProject from '../pages/projects/PokemonProject';
 function App() {
   return (
     <div className="App">
-
       
-
       <BrowserRouter>
-
         {/* This is our custom navbar! */}
         <Navbar />
-
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/about' element={<AboutPage />} />
           <Route path='/contact' element={<ContactPage />} />
+          <Route path="/projects" element={<ProjectLayout />}>
+            <Route path="/projects/pokemon" element={<PokemonProject />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
   );
 }
-
 export default App;
